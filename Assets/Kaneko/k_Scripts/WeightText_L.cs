572da81b6bc4af0063cc81e2057eb595c0ada79e
Arata <arata.orange.music@gmail.com>
2020-07-01 16:38:26 +0900

    feature_巨獣の仕様実装Ver02
    巨獣はコピペでOK
    Canvasとイベントシステムを追加。Canvasの中のTextとText(1)のスクリプトのゲームオブジェクトの箱に、Kyojyuオブジェクトを入れといてください

---
+++ Assets/Kaneko/k_Scripts/WeightText_L.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeightText_L : MonoBehaviour
+{
+    private Text targetText;
+    public GameObject Kyojyu;
+    private float distance_UI;
+
+    float weight;
+
+    void Start()
+    {
+        this.targetText = this.GetComponent<Text>();
+
+    }
+
+    void Update()
+    {
+        weight = Kyojyu.GetComponent<KyojyuManager>().L_weight;
+        this.targetText.text = "L：" + weight;
+    }
+}
